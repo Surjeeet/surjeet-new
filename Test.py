34ced865906b10b73b8ea983069a059cfb43cc6c
Jeet13 <Surjeet.Singh3RCE@triosstudent.com>
2023-04-04 12:37:06 -0400

    v22

--- Test.py
+++ Test.py
@@ -1,28 +1,15 @@
-Month = int(input ("Enter the month in the numeric form."))
-date = int(input ("enter date"))
-year = int(input ("enter year"))
-dd=1
-
-if Month >0 and Month <=12:
-    dd = 1
+a = int(input("Enter the month in numerical form :"))
+if(a>12 or a<0):
+  print("Error: Invalid Month Input")
+  exit(1)
+b = int(input("Enter the day in numeric form :"))
+if (b>31 or b<0):
+  print("Error: Invalid Day Input")
+  exit(1)
+c =int(input("Enter the year as two numerical digits :"))
+if (c>99 or c<0):
+  print("Error: Invalid Year Input")
+  exit(1)
 else:
-    dd=0
-    print("Error: Invalid Month Input")
-
-
-if date >0 and date <=31:
-    dd = 1
-else:
-    dd=0
-    print("Error: Invalid Date Input")
-
-if year >0 and year <=99:
-    dd = 1
-else:
-    dd=0
-
-if dd == 0:
-    print("invalid")
-else:
- print("Success: Congratulations! you entered the correct date.") 
- print("the date is",Month,"/",date,"/",year)
+  print( a, "/", b, "/", c)
+  print("Success: Congratulations! you entered the correct date.")
