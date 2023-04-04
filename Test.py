d0967f2130e8c093a7b7c765bf42a980a04b8629
Jeet13 <Surjeet.Singh3RCE@triosstudent.com>
2023-04-04 12:22:51 -0400

    v1

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-Month = int(input ("enter month"))
+Month = int(input ("Enter the month in the numeric form."))
 date = int(input ("enter date"))
 year = int(input ("enter year"))
 dd=1
@@ -7,12 +7,14 @@ if Month >0 and Month <=12:
     dd = 1
 else:
     dd=0
+    print("Error: Invalid Month Input")
 
 
 if date >0 and date <=31:
     dd = 1
 else:
     dd=0
+    print("Error: Invalid Date Input")
 
 if year >0 and year <=99:
     dd = 1
